14cb2d4dd8af152d273cdaa5137f2515ff336c11
Atul Kumar Nag <atulnag12@gmail.com>
2025-07-12 11:52:07 +0530

    Adding router-dom.

---
+++ frontend/elearning/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="home">
+    <h2>Welcome to AKNTutorials</h2>
+    <p>Your trusted platform for online learning.</p>
+  </div>
+);
+
+export default Home;
